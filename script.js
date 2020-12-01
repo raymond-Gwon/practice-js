ba8005a36e76a747985b8a40e92fd9ebef64c99b
raymond-Gwon <73869660+raymond-Gwon@users.noreply.github.com>
2020-12-01 21:18:42 +0900

    DreamCoding1

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
-const title = document.querySelector("#title");
+const toggleBtn = document.querySelector('.navbar__toogleBtn');
+const menu = document.querySelector('.navbar__menu');
+const icons = document.querySelector('.navbar__icons');
 
-const BASE_COLOR = "rgb(52, 73, 94)";
-const OTHER_COLOR = "#7f8c8d";
-
-
-function handleClick() {
-    const currentColor = title.style.color;
-    if (currentColor === BASE_COLOR){
-    title.style.color = OTHER_COLOR;
-    }    
-    else {
-    title.style.color = BASE_COLOR;
-    }
-}
-
-function init() {
-    title.style.color =BASE_COLOR;
-    title.addEventListener("mouseenter", handleClick);
-}
-init();
+toggleBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    icons.classList.toggle('active');
+});
